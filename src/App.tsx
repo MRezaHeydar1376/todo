5fcb8756b7c29fbe43e1cc46ad4592f028239086
MRezaHeydar1376 <103419484+MRezaHeydar1376@users.noreply.github.com>
2022-12-13 15:38:50 -0800

    Add Global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import { Global, css } from '@emotion/react'
 import React from 'react';
+import Home from './pages';
+import { GlobalStyles } from './styles';
 
 function App() {
   return (
     <div>
-      hello world!
+      <Global
+        styles={css`
+         body{
+            margin: 0;
+            padding: 0;
+          }
+        `}
+      />
+      <Home />
     </div>
   );
 }
