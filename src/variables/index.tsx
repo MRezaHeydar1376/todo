424bbef929fe04bf0380632d299dc1a315e89e9e
MRezaHeydar1376 <103419484+MRezaHeydar1376@users.noreply.github.com>
2022-12-13 15:40:21 -0800

    Create section Color

---
+++ src/variables/index.tsx
@@ -0,0 +1,11 @@
+export const Color = {
+    white: "#FFFFFF",
+    black: "#000000",
+    green: "#0ad188",
+    red: "#db1f28",
+    yellow: "#f5ed11",
+    blue: "#214fdb",
+    gray: "#808696",
+    darkGray: "#D4CACA",
+    darkWhite: "#e4e8f5",
+}
