aa25b411dfd4f1dfd06ae7fb5e20f33834fdcf70
MRezaHeydar1376 <103419484+MRezaHeydar1376@users.noreply.github.com>
2022-12-13 15:40:38 -0800

    Create Navbar

---
+++ src/layouts/navbar.tsx
@@ -0,0 +1,11 @@
+import { Div } from "../styles";
+
+function Navbar() {
+    return (
+        <Div width="100%" height="50px" backgroundColor="red">
+            
+        </Div>
+    );
+}
+
+export default Navbar;
