33e4330fce6278298f367524fc2371994d8e86d0
MRezaHeydar1376 <103419484+MRezaHeydar1376@users.noreply.github.com>
2022-12-14 16:01:49 -0800

    Add new color

--- src/variables/index.tsx
+++ src/variables/index.tsx
@@ -1,9 +1,11 @@
 export const Color = {
     white: "#FFFFFF",
     black: "#000000",
+    blackBlue: "#002D62",
     green: "#0ad188",
     red: "#db1f28",
     yellow: "#f5ed11",
+    blackYellow: "#FFC72C",
     blue: "#214fdb",
     gray: "#808696",
     darkGray: "#D4CACA",
