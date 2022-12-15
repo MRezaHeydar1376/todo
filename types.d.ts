f225baf1cf91afb76ee6d3d58289f8b196ba88df
MRezaHeydar1376 <103419484+MRezaHeydar1376@users.noreply.github.com>
2022-12-14 16:00:10 -0800

    Add file and setting for all type images

---
+++ types.d.ts
@@ -0,0 +1,12 @@
+declare module "*.png" {
+    const src: string;
+    export default src;
+}
+declare module "*.svg" {
+    const svg: string;
+    export default svg;
+}
+declare module "*.jpg" {
+    const src: string;
+    export default jpg;
+}
