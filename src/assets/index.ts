91b9f36136a014801b5f4326fd15f5e31315ef9d
MRezaHeydar1376 <103419484+MRezaHeydar1376@users.noreply.github.com>
2022-12-15 09:20:11 -0800

    Add new icons

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,3 +1,5 @@
 export { default as Setting } from "./icons/setting.svg";
 export { default as Sun } from "./icons/sun.svg";
 export { default as Moon } from "./icons/moon.svg";
+export { default as IranFlag } from "./icons/iran-flag.svg";
+export { default as UsaFlag } from "./icons/usa-flag.svg";
