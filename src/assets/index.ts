15d1514543368a0fe96cf263886f90f52e9fdf12
MRezaHeydar1376 <103419484+MRezaHeydar1376@users.noreply.github.com>
2022-12-28 05:26:21 -0800

    add new images

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -3,3 +3,5 @@ export { default as Sun } from "./icons/sun.svg";
 export { default as Moon } from "./icons/moon.svg";
 export { default as IranFlag } from "./icons/iran-flag.svg";
 export { default as UsaFlag } from "./icons/usa-flag.svg";
+export { default as BackgroundLight } from "./images/background-light.webp";
+export { default as BackgroundDark } from "./images/background-dark.webp";
