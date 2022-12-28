feb8607ccae5cab9eca54a10c12b8e60af62c4f7
MRezaHeydar1376 <103419484+MRezaHeydar1376@users.noreply.github.com>
2022-12-28 08:02:17 -0800

    Add prop editTask

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,6 +1,7 @@
-import { ChangeEvent } from "react";
-import { Trash } from "../assets";
-import { Div, Img, Input, Line, Paragraph } from "../styles";
+import { ChangeEvent, useState } from "react";
+import { Close, Edit, Trash } from "../assets";
+import { Button, Div, H5, H6, Img, Input, Line, Paragraph } from "../styles";
+import { Color } from "../variables";
 
 interface Props {
     id: string;
@@ -8,47 +9,118 @@ interface Props {
     isCompleted: boolean;
     completeTask(): void;
     removeTask(id: string): void;
+    editTask(id: string, text: string): void;
 }
 
-function Card({ id ,text, isCompleted, completeTask, removeTask }: Props) {
+function Card({ id, text, isCompleted, completeTask, removeTask, editTask }: Props) {
+
+    const [task, setTask] = useState("");
+    const [edit, setEdit] = useState(false);
+
+    function getValue(event: ChangeEvent<HTMLInputElement>) {
+        setTask(event.target.value);
+    };
+
+    function submit() {
+        if (task.trim() !== "") {
+            editTask(id, task)
+            setTask("")
+            setEdit(false)
+        }
+        else {
+            alert("Please write text")
+        }
+    };
+
+    function showEdit() {
+        setEdit(true)
+    };
+
+    function closeEdit() {
+        setEdit(false)
+    };
 
     return (
         <>
             <Div height="80px" borderRadius="8px" display="flex">
-                <Div
-                    width="80%"
-                    padding="4px 8px"
-                    overflowY="auto"
-                    overflowX="hidden"
-                >
-                    <Paragraph fontSize="12px" fontWeight="400">
-                        {text}
-                    </Paragraph>
-                </Div>
-                <Line height="95%" width="0.01%" margin="auto 0" />
-                <Div
-                    width="19%"
-                    height="100%"
-                    display="flex"
-                    justify="center"
-                    align="center"
-                >
-                    <Input
-                        type="checkbox"
-                        width="30%"
-                        height="30%"
-                        checked={isCompleted}
-                        onChange={completeTask}
-                        margin="0 10px"
-                    />
-                    <Img
-                        src={Trash}
-                        width="20px"
-                        height="25px"
-                        cursor="pointer"
-                        onClick={() => removeTask(id)}
-                    />
-                </Div>
+                {edit ? (
+                    <Div display="flex" align="center" height="100%">
+                        <Input
+                            border={`1px solid ${Color.blue}`}
+                            borderRadius="8px 0 0 8px"
+                            height="40px"
+                            padding="0 8px"
+                            placeholder="Write task..."
+                            onChange={getValue}
+                            value={task}
+                        />
+                        <Button
+                            width="120px"
+                            height="40px"
+                            border="none"
+                            borderRadius="0 8px 8px 0"
+                            backgrondColor={Color.green}
+                            onClick={submit}
+                        >
+                            <H5 cursor="pointer" fontSize="12px" fontWeight="700">
+                                EDIT
+                            </H5>
+                        </Button>
+                        <Button
+                            width="55px"
+                            height="40px"
+                            border="none"
+                            onClick={closeEdit}
+                        >
+                            <Img src={Close} width="30px"/>
+                        </Button>
+                    </Div>
+                ) : (
+                    <>
+                        <Div
+                            width="80%"
+                            padding="4px 8px"
+                            height="90%"
+                            overflowY="auto"
+                            overflowX="hidden"
+                        >
+                            <Paragraph fontSize="12px" fontWeight="400">
+                                {text}
+                            </Paragraph>
+                        </Div>
+                        <Line height="95%" width="0.01%" margin="auto 0" />
+                        <Div
+                            width="19%"
+                            height="100%"
+                            display="flex"
+                            justify="space-between"
+                            align="center"
+                            padding="0 20px"
+                        >
+                            <Input
+                                type="checkbox"
+                                width="30%"
+                                height="30%"
+                                checked={isCompleted}
+                                onChange={completeTask}
+                            />
+                            <Img
+                                src={Trash}
+                                width="20px"
+                                height="25px"
+                                cursor="pointer"
+                                onClick={() => removeTask(id)}
+                            />
+                            <Img
+                                src={Edit}
+                                width="20px"
+                                height="25px"
+                                cursor="pointer"
+                                onClick={showEdit}
+                            />
+                        </Div>
+                    </>
+                )}
             </Div>
             <Line width="99%" margin="0 auto" />
         </>
