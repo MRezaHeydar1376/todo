60db5c5b7a97e90cb519257d1f8011035ef73dcf
MRezaHeydar1376 <103419484+MRezaHeydar1376@users.noreply.github.com>
2023-01-30 07:40:25 -0800

    Add interface CustomTheme to colors

--- src/variables/index.tsx
+++ src/variables/index.tsx
@@ -1,4 +1,20 @@
-export const Color = {
+export interface CustomTheme {
+    white: string;
+    black: string;
+    blackBlue: string;
+    green: string;
+    lime: string;
+    red: string;
+    yellow: string;
+    blackYellow: string;
+    blue: string;
+    gray: string;
+    darkGray: string;
+    darkWhite: string;
+}
+
+
+export const themeLight: CustomTheme = {
     white: "#FFFFFF",
     black: "#000000",
     blackBlue: "#002D62",
@@ -8,6 +24,21 @@ export const Color = {
     yellow: "#f5ed11",
     blackYellow: "#FFC72C",
     blue: "#214fdb",
+    gray: "#808696",
+    darkGray: "#D4CACA",
+    darkWhite: "#e4e8f5",
+}
+
+export const themeDark: CustomTheme = {
+    black: "#FFFFFF",
+    white: "#000000",
+    blackBlue: "#002D62",
+    green: "#0ad188",
+    lime: "#00FF00",
+    blue: "#214fdb",
+    yellow: "#f5ed11",
+    blackYellow: "#FFC72C",
+    red: "#db1f28",
     gray: "#808696",
     darkGray: "#D4CACA",
     darkWhite: "#e4e8f5",
