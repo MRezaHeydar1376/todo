9d63caa81adbc8fbaa4a4083f0e3e02dae4c0dda
MRezaHeydar1376 <103419484+MRezaHeydar1376@users.noreply.github.com>
2023-01-30 07:39:17 -0800

    Add new icons

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,10 +1,12 @@
 export { default as Setting } from "./icons/setting.svg";
+export { default as SettingWhite } from "./icons/setting-white.svg";
 export { default as Sun } from "./icons/sun.svg";
 export { default as Moon } from "./icons/moon.svg";
 export { default as IranFlag } from "./icons/iran-flag.svg";
 export { default as UsaFlag } from "./icons/usa-flag.svg";
 export { default as Trash } from "./icons/trash.svg";
 export { default as Edit } from "./icons/edit.svg";
+export { default as EditWhite } from "./icons/edit-white.svg";
 export { default as Close } from "./icons/close.svg";
 export { default as BackgroundLight } from "./images/background-light.webp";
 export { default as BackgroundDark } from "./images/background-dark.webp";
